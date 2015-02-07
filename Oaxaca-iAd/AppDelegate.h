84dcfc08683f4df5485dd6ac3bd6efee83c643da
lab04 <ieepo@felipe.local>
2015-02-07 09:55:48 -0600

    Initial Commit

---
+++ Oaxaca-iAd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Oaxaca-iAd
+//
+//  Created by felipe on 07/02/15.
+//  Copyright (c) 2015 felipe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
